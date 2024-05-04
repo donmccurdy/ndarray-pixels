fa2c49aaba06a9328779271180c7a7c8dd853986
Dan <DanOfTheRoses@gmail.com>
2024-05-04 15:45:06 -0500

    Use OffscreenCanvas

--- src/browser-get-pixels.ts
+++ src/browser-get-pixels.ts
@@ -20,9 +20,7 @@ export function getPixelsInternal(
 		img.onload = function () {
 			URL.revokeObjectURL(path as string);
 
-			const canvas = document.createElement('canvas');
-			canvas.width = img.width;
-			canvas.height = img.height;
+			const canvas = new OffscreenCanvas(img.width, img.height)
 			const context = canvas.getContext('2d')!;
 			context.drawImage(img, 0, 0);
 			const pixels = context.getImageData(0, 0, img.width, img.height);
